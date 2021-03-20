45a6089a3ad97f2d2331efd998d1890cdc7204e1
sidux <ahmed.lebbada@openclassrooms.com>
2021-03-20 20:52:15 +0100

    fix var name

--- util.zsh
+++ util.zsh
@@ -3,17 +3,17 @@
 function bundle() {
   local repo=$1
   local repo_url="https://github.com/$repo.git"
-  local path="${ZDOTDIR:-$HOME}/.zsh/$repo"
-  local name="${path##*/}"
+  local repo_path="${ZDOTDIR:-$HOME}/.zsh/$repo"
+  local name="${repo_path##*/}"
 
-  declare -a arr=("$path/init.zsh" "$path/init.sh" "$path/$name.zsh" "$path/$name.sh")
+  declare -a arr=("$repo_path/init.zsh" "$repo_path/init.sh" "$repo_path/$name.zsh" "$repo_path/$name.sh")
 
-  if [[ ! -d "$path" ]]; then
+  if [[ ! -d "$repo_path" ]]; then
     if ! git ls-remote -q "$repo_url" >/dev/null; then
       echo "Module $repo does not exist."
       return 1
     fi
-    git clone --recursive "$repo_url" "$path"
+    git clone --recursive "$repo_url" "$repo_path"
   fi
 
   for file in "${arr[@]}"; do
