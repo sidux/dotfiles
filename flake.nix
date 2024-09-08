9216027f31c40ccc6c3b53a71d8c17ecdf915433
sidux <ahmed.lebbada@gmail.com>
2024-09-08 14:34:09 +0200

    update flake adding macos settings

--- flake.nix
+++ flake.nix
@@ -1,83 +1,175 @@
 {
-  description = "Example Darwin system flake";
+  description = "Example Darwin system flake with home-manager";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, darwin, nixpkgs, home-manager }:
   let
-    configuration = { pkgs, ... }: {
-
-      nix = {
-        settings = {
-          experimental-features = [ "nix-command" "flakes" "repl-flake" ];
-          bash-prompt-prefix = "(nix:$name)\040";
-          max-jobs = "auto";
-          extra-nix-path = "nixpkgs=flake:nixpkgs";
-          trusted-users = [ "root" "ahmed" ];
-        };
-        buildMachines = [{
-          hostName = "build-users-group";
-          systems = [ "nixbld" ];
-        }];
-      };
-
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs; [
-          git
-          gh
-          htop
-          jq
-          yq-go
-          nodejs_20
-          nixpkgs-fmt
-          mysides
-      ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      programs.fish.enable = true;
-
-      programs.zsh.enable = true;  # We still need this for macOS
-      environment.shells = [ pkgs.fish ];
-
-      users.users.${builtins.getEnv "USER"} = {
-        shell = pkgs.fish;
-      };
-
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      security.pam.enableSudoTouchIdAuth = true;
-      programs.direnv.enable = true;
-      programs.tmux.enableFzf = true;
-      programs.vim.enable = true;
-      # programs.zsh.enableFzfCompletion = true;
-      # programs.zsh.enableFzfHistory = true;
-
-    };
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+    user = "<user>";
+    host = "<host>";
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#my_hostname
-    darwinConfigurations."my_hostname" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+    darwinConfigurations."${host}" = darwin.lib.darwinSystem {
+      inherit system;
+      modules = [
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.${user} = { pkgs, ... }: {
+            home.stateVersion = "24.05";
+
+            programs.git = {
+              enable = true;
+              userName = "sidux";
+              userEmail = "ahmed.lebbada@gmail.com";
+              ignores = [ ".DS_Store" "*.swp" ];
+              extraConfig = {
+                core = {
+                  editor = "code";
+                };
+                color = {
+                  ui = true;
+                };
+              };
+            };
+
+            programs.vscode = {
+              enable = true;
+              package = pkgs.vscode;
+            };
+
+            fonts.fontconfig.enable = true;
+
+            # Copy a custom file
+            # home.file.".custom_file".source = ./path/to/your/custom_file;
+
+            # You can add more home-manager configurations here
+          };
+        }
+        ({ pkgs, ... }: {
+          nixpkgs.hostPlatform = "aarch64-darwin";
+          nixpkgs.config.allowUnfree = true;
+
+          # Your system configuration
+          environment.systemPackages = with pkgs; [
+            htop
+            devenv
+            jq
+            yq-go
+            nodejs_20
+            nixpkgs-fmt
+            vscode
+            iterm2
+            jetbrains-mono
+          ];
+          services.nix-daemon.enable = true;
+          nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+
+          programs.direnv.enable = true;
+
+          programs.zsh.enable = true;
+
+          programs.fish = {
+            enable = true;
+            useBabelfish = true;
+          };
+
+
+          users.users.${user} = {
+            name = user;
+            home = "/Users/${user}";
+            shell = pkgs.fish;
+          };
+
+          environment.shells = with pkgs; [ zsh fish ];
+
+          system.stateVersion = 4;
+
+          security.pam.enableSudoTouchIdAuth = true;
+
+          system.defaults = {
+            ".GlobalPreferences"."com.apple.mouse.scaling" = 4.0;
+            spaces.spans-displays = true;
+
+            dock = {
+              autohide = true;
+              autohide-delay = 0.0;
+              autohide-time-modifier = 0.0;
+              orientation = "bottom";
+              dashboard-in-overlay = true;
+              largesize = 85;
+              tilesize = 50;
+              magnification = true;
+              launchanim = false;
+              mru-spaces = false;
+              show-recents = false;
+              show-process-indicators = false;
+              static-only = true;
+              mouse-over-hilite-stack = true;
+              wvous-tl-corner = 2;
+              wvous-bl-corner = 4;
+              # wvous-tl-modifier = 0;
+              # wvous-bl-modifier = 0;
+            };
+
+            finder = {
+              AppleShowAllExtensions = true;
+              AppleShowAllFiles = true;
+              CreateDesktop = false;
+              FXDefaultSearchScope = "SCcf"; # current folder
+              QuitMenuItem = true;
+              ShowPathbar = true;
+              _FXShowPosixPathInTitle = true;
+              # NewWindowTarget = "PfHm";
+              # NewWindowTargetPath = "file:///Users/${user}/";
+              # _FXSortFoldersFirst = true;
+            };
+
+            NSGlobalDomain = {
+              _HIHideMenuBar = false;
+              AppleFontSmoothing = 0;
+              AppleInterfaceStyle = "Dark";
+              AppleKeyboardUIMode = 3;
+              AppleScrollerPagingBehavior = true;
+              AppleShowAllExtensions = true;
+              AppleShowAllFiles = true;
+              InitialKeyRepeat = 10;
+              KeyRepeat = 2;
+              NSAutomaticSpellingCorrectionEnabled = false;
+              NSAutomaticWindowAnimationsEnabled = false;
+              # Expand save panel by default
+              NSNavPanelExpandedStateForSaveMode = true;
+              NSNavPanelExpandedStateForSaveMode2 = true;
+              NSWindowResizeTime = 0.0;
+              # Disable “natural” scrolling
+              "com.apple.swipescrolldirection" = false;
+              "com.apple.sound.beep.feedback" = 0;
+              "com.apple.trackpad.scaling" = 2.0;
+              # Increase sound quality for Bluetooth headphones/headsets
+              # "com.apple.BluetoothAudioAgent" = {
+              #   "Apple Bitpool Min (editable)" = 40;
+              # };
+            };
+          };
+        })
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."my_hostname".pkgs;
+    darwinPackages = self.darwinConfigurations."${host}".pkgs;
   };
 }
--- nix_rebuild.sh
+++ nix_rebuild.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-set -e  # Exit immediately if a command exits with a non-zero status.
-
 # Path to your flake.nix file
 FLAKE_FILE="$HOME/dotfiles/flake.nix"
 
 # Get the current hostname
 CURRENT_HOSTNAME=$(hostname -s)
+CURRENT_USER=$(whoami)
 
 echo "Current hostname: $CURRENT_HOSTNAME"
 
 # Temporary file for sed operation
 TEMP_FILE=$(mktemp)
 
-# Replace "my_hostname" with the current hostname
-sed "s/\"my_hostname\"/\"$CURRENT_HOSTNAME\"/g" "$FLAKE_FILE" > "$TEMP_FILE"
+sed -e "s/\"<host>\"/\"$CURRENT_HOSTNAME\"/g" \
+    -e "s/\"<user>\"/\"$CURRENT_USER\"/g" "$FLAKE_FILE" > "$TEMP_FILE"
 mv "$TEMP_FILE" "$FLAKE_FILE"
 
 echo "Hostname updated in flake.nix"
@@ -26,11 +25,10 @@ if [ -e ./result/sw/bin/darwin-rebuild ]; then
     ./result/sw/bin/darwin-rebuild switch --flake ".#$CURRENT_HOSTNAME"
 else
     echo "Error: darwin-rebuild not found in expected location. The build may have failed."
-    exit 1
 fi
 
-# Replace the hostname back to "my_hostname"
-sed "s/\"$CURRENT_HOSTNAME\"/\"my_hostname\"/g" "$FLAKE_FILE" > "$TEMP_FILE"
+sed -e "s/\"$CURRENT_HOSTNAME\"/\"<host>\"/g" \
+    -e "s/\"$CURRENT_USER\"/\"<user>\"/g" "$FLAKE_FILE" > "$TEMP_FILE"
 mv "$TEMP_FILE" "$FLAKE_FILE"
 
-echo "Configuration updated and hostname restored in flake.nix"
+echo "Hostname restored in flake.nix"
